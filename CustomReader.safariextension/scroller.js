84228954894c1629b6ae2695cb30362cf7a5b2f2
Chul Kim <ckim@omniupdate.com>
2015-03-17 07:43:58 -0700

    Reduced j/k scroll increment.

--- CustomReader.safariextension/scroller.js
+++ CustomReader.safariextension/scroller.js
@@ -20,10 +20,10 @@ function scrollSmoothly(e) {
 		case 32: 	// space
 			e.preventDefault(); e.stopPropagation();
 			smoothScroll(scrollElement, (longScrollLength * (e.shiftKey ? -1 : 1)), longScrollTime); break;
-		case 33: case 75: 	// pg up, k
+		case 33: 	// pg up
 			e.preventDefault(); e.stopPropagation();
 			smoothScroll(scrollElement, (-1 * longScrollLength), longScrollTime); break;
-		case 34: case 74: 	// pg dn, j
+		case 34: 	// pg dn
 			e.preventDefault(); e.stopPropagation();
 			smoothScroll(scrollElement, longScrollLength, longScrollTime); break;
 		case 35: 	// end
@@ -32,10 +32,16 @@ function scrollSmoothly(e) {
 			break;
 		case 38: 	// up
 			e.preventDefault(); e.stopPropagation();
-			smoothScroll(scrollElement, (-1 * 50), shortScrollTime); break;
+			smoothScroll(scrollElement, -50, shortScrollTime); break;
 		case 40: 	// down
 			e.preventDefault(); e.stopPropagation();
 			smoothScroll(scrollElement, 50, shortScrollTime); break;
+		case 74:    // k
+			e.preventDefault(); e.stopPropagation();
+			smoothScroll(scrollElement, 60, shortScrollTime); break;
+		case 75:    // j
+			e.preventDefault(); e.stopPropagation();
+			smoothScroll(scrollElement, -60, shortScrollTime); break;
 		default: break;
 	}
 }
